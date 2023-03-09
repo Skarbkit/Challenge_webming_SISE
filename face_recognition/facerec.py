802ae14d6f09f0101348fe07b4733637b11488db
HugoUrba <114567685+HugoUrba@users.noreply.github.com>
2023-03-09 11:53:06 +0100

    modifs pr prénoms

--- face_recognition/facerec.py
+++ face_recognition/facerec.py
@@ -4,11 +4,15 @@ import numpy as np
 import threading
 import face_recognition
 import time
+import pickle
 #import torch
 
-
-# Fonction pour charger les modèles de détection de genre, d'âge et d'émotions
-
+def load_known_data():
+    data = pickle.loads(open("face_enc", "rb").read())
+    return (
+        data["names"], 
+        data["encodings"]
+        )
 # fonction qui va charger le modèle de détection de genre
 def load_genre_detection_model():
     model = cv2.dnn.readNetFromCaffe("gender_deploy.prototxt", "gender_net.caffemodel")
@@ -31,57 +35,7 @@ def load_all_models():
 
 
 
-# Fonction pour la détection de visage, d'âge, de genre et d'émotions
-def detect_face(img, model_age, model_genre):
-    # Convertir l'image en RGB
-    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # Détecter les visages dans l'image
-    faces = face_recognition.face_locations(rgb, model='hog')
-
-    # Pour chaque visage détecté, détecter l'âge, le genre et les émotions
-    for (top, right, bottom, left) in faces:
-        # Dessiner un rectangle autour du visage détecté
-        cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 2)
-
-        # Détecter l'âge
-        #age_model = cv2.dnn.readNetFromCaffe('age_deploy.prototxt', 'age_net.caffemodel')
-        age_model = model_age
-        blob = cv2.dnn.blobFromImage(img, scalefactor=1.0, size=(227, 227), mean=(78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
-        age_model.setInput(blob)
-        age_preds = age_model.forward()
-        age = round(np.sum(age_preds[0] * np.arange(0, 8)) * 4)
-
-
-        # Détecter le genre
-        #gender_model = cv2.dnn.readNetFromCaffe('gender_deploy.prototxt', 'gender_net.caffemodel')
-        gender_model = model_genre
-        blob = cv2.dnn.blobFromImage(img, scalefactor=1.0, size=(227, 227), mean=(78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
-        gender_model.setInput(blob)
-        gender_preds = gender_model.forward()
-        # changer le genre en fonction de la prédiction avec < alors que c'est > à l'origine
-        gender = 'Homme' if gender_preds[0][0] > gender_preds[0][1] else 'Femme'
-
-        # # Détecter les émotions
-        # emotion_model = cv2.dnn.readNetFromTensorflow('emotion_frozen.pb')
-        # face = img[top:bottom, left:right]
-        # blob = cv2.dnn.blobFromImage(face, 1.0, (64, 64), (104.0, 177.0, 123.0), False, False)
-        # emotion_model.setInput(blob)
-        # emotion_preds = emotion_model.forward()
-        # emotion_labels = ['Enervé', 'Dégouté', 'Apeuré', 'Heureux', 'Neutre', 'Triste', 'Surpris']
-        # emotion = emotion_labels[np.argmax(emotion_preds)]
-
-
-        # Afficher les résultats
-        cv2.putText(img, f'Age: {age}', (left, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-        cv2.putText(img, f'Genre: {gender}', (left, bottom+25), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-        #cv2.putText(img, f'Emotion: {dominant_emotion}', (right-30, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        #cv2.putText(img, f'Score: {emotion_score}', (right, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-    return img
-
-
-def detect_face_opti(img, age_model, gender_model):
+def detect_face_opti(img, age_model, gender_model, known_names, known_encodings):
     # Convertir l'image en RGB
     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -111,6 +65,22 @@ def detect_face_opti(img, age_model, gender_model):
         # Dessiner un rectangle autour du visage détecté
         cv2.rectangle(img, (left, top), (right, bottom), (255, 0, 0), 2)
 
+        # Récupérer les coordonnées du visage détecté
+        for i in range(len(known_encodings)):
+            # Comparer chaque visage trouvé avec tous les visages connus
+            matches = face_recognition.compare_faces(known_encodings[i], faces, tolerance=0.7)
+            # Trouver le nom correspondant pour chaque visage correspondant
+            name = "Inconnu"
+            if True in matches:
+                # Trouver les index de toutes les correspondances
+                match_indexes = [i for (i, b) in enumerate(matches) if b]
+                # Compter toutes les correspondances et trouver l'index avec le plus grand nombre de correspondances
+                counts = {}
+                for i in match_indexes:
+                    name = known_names[i]
+                    counts[name] = counts.get(name, 0) + 1
+                    name = max(counts, key=counts.get)
+
 
         # Détecter l'âge
         age_model.setInput(blob)
@@ -125,15 +95,19 @@ def detect_face_opti(img, age_model, gender_model):
         # Afficher les résultats
         cv2.putText(img, f'Age: {age}', (left, top-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
         cv2.putText(img, f'Genre: {gender}', (left, bottom+25), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        cv2.putText(img, f"Nom: {name}", (left, bottom+50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+
 
     return img
 
 
 # Fonction pour la lecture en continu de la webcam
-def video_capture(model_age, model_genre):
+def video_capture(model_age, model_genre,know_names,known_encodings):
     # Charger les modèles
     model_age = model_age
     model_genre = model_genre
+    known_names = know_names
+    known_encodings = known_encodings
 
 
 
@@ -159,7 +133,7 @@ def video_capture(model_age, model_genre):
 
             # Appeler la fonction de détection de visage dans un thread séparé seulement une image sur deux pour accélérer le traitement
             #if count % 2 == 0:
-            thread = threading.Thread(target=detect_face_opti, args=(frame,model_age,model_genre))
+            thread = threading.Thread(target=detect_face_opti, args=(frame,model_age,model_genre, know_names, known_encodings))
             thread.start() # Lancer le thread
             thread.join() # Attendre la fin du thread pour continuer
              # Attendre la fin du thread pour continuer
@@ -188,9 +162,10 @@ def main():
 
     # Charger les modèles
     model_age, model_genre = load_all_models()
+    known_names, known_encodings = load_known_data()
 
 
-    video_capture(model_age, model_genre)
+    video_capture(model_age, model_genre, known_names, known_encodings)
 
 if __name__ == '__main__':
     main()
